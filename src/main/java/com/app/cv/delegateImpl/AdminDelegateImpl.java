1866e9fa40f3255994d675b4ebcb7e8767d2f324
sharad <sharadchavhan001@gmail.com>
2024-08-18 02:00:20 +0530

    api.yml added for admin Operations

--- src/main/java/com/app/cv/delegateImpl/AdminDelegateImpl.java
+++ src/main/java/com/app/cv/delegateImpl/AdminDelegateImpl.java
@@ -1,9 +1,6 @@
 package com.app.cv.delegateImpl;
 
 import com.app.cv.api.AdminApiDelegate;
-import com.app.cv.model.Admin;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.NativeWebRequest;
 
@@ -14,11 +11,5 @@ public class AdminDelegateImpl implements AdminApiDelegate {
     @Override
     public Optional<NativeWebRequest> getRequest() {
         return AdminApiDelegate.super.getRequest();
-    }
-
-    @Override
-    public ResponseEntity<Admin> getAdminDetails() {
-        System.out.println("getAdminDetails()");
-        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
